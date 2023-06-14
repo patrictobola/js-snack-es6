8be9a9f0f6bebf855ddce653725f1101b86ffc3a
patrictobola <tobola92@gmail.com>
2023-06-14 15:05:21 +0200

    snack 4 done

---
+++ js/snack3.js
@@ -0,0 +1,29 @@
+const bikes = [
+    {
+        name: "bike1",
+        weight: 3,
+    },
+    {
+        name: "bike2",
+        weight: 1,
+    },
+    {
+        name: "bike3",
+        weight: 5,
+    },
+    {
+        name: "bike4",
+        weight: 7,
+    },
+    {
+        name: "bike5",
+        weight: 2,
+    },
+    {
+        name: "bike6",
+        weight: 15,
+    }
+]
+// console.log(bikes)
+// const lessWeight = bikes.reduce((accumulator, element) => accumulator.weight < element.weight ? accumulator : element, 0);
+// console.log(lessWeight)
---
+++ js/snack4.js
@@ -0,0 +1,57 @@
+
+
+const teams = [
+    {
+        name: "squadra1",
+        score: 0,
+        fouledAgainst: 0,
+    },
+    {
+        name: "squadra2",
+        score: 0,
+        fouledAgainst: 0,
+    },
+    {
+        name: "squadra3",
+        score: 0,
+        fouledAgainst: 0,
+    },
+    {
+        name: "squadra4",
+        score: 0,
+        fouledAgainst: 0,
+    },
+    {
+        name: "squadra5",
+        score: 0,
+        fouledAgainst: 0,
+    },
+    {
+        name: "squadra6",
+        score: 0,
+        fouledAgainst: 0,
+    }
+]
+
+
+// Creo una variabile dove generare numeri casuali da 1 a max 
+const randomNumber = (max) => Math.floor(Math.random() * max) + 1;
+
+console.log("Originale")
+console.log("---------------")
+console.log(teams)
+// Genero e aggiungo nell'array un numero casuale di goal e falli subiti per ogni squadra 
+for (let team of teams){
+    team.score = randomNumber(50);
+    team.fouledAgainst = randomNumber(10)
+}
+
+const updatedTeams = teams.map(({name, fouledAgainst}) => (
+    {
+        name,
+        fouledAgainst
+    }
+));
+console.log("---------------")
+console.log("Aggiornato")
+console.log(updatedTeams)
